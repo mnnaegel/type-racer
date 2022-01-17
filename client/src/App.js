83861da86155dd5e5828b1e3a909cd4eb844bae5
mnnaegel <86453692+mnnaegel@users.noreply.github.com>
2022-01-17 00:25:11 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,35 @@
-import React,{useEffect} from 'react';
+import React,{useEffect,useState} from 'react';
 import { Router, Switch, Route } from 'react-router-dom'
 import history from './history';
 import GameMenu from './components/GameMenu';
+import CreateGame from './components/createGame';
+import JoinGame from './components/JoinGame'
 import socket from './socketConfig'
 
 function App() {
+  const [gameState, setGameState] = useState({_id:"",isOpen:false,players:[],words:[]})
   useEffect(()=>{
-    socket.on('test',msg=>{
-      console.log(msg)
+    socket.on('updateGame',(game)=>{
+      console.log(game);
+      setGameState(game)
     })
+    return ()=>{
+      socket.removeAllListeners()
+    }
   },[])
+
+  useEffect(()=>{
+    if(gameState._id !== "")
+      history.push(`/game/${gameState._id}`)
+  },[gameState._id])
+  
   return (
     <div>
       <Router history={history}>
         <Switch>
           <Route path="/" exact component={GameMenu} />
+          <Route path="/game/create" component={CreateGame} />
+          <Route path="/game/join" component={JoinGame} />
         </Switch>
       </Router>   
     </div>
