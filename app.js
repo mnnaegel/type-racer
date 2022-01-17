a7055c7888ad0f87e9a4c8e4aafd47aa51eb6883
mnnaegel <86453692+mnnaegel@users.noreply.github.com>
2022-01-16 22:10:04 -0500

    socketio front end and backend

--- app.js
+++ app.js
@@ -4,7 +4,17 @@ const socketio = require('socket.io')
 const mongoose = require('mongoose')
 
 const expressServer = app.listen(3001)
-const io = socketio(expressServer);
+// const io = socketio(expressServer);
+const io=socketio(expressServer,{
+    cors:{
+        origin:'http://localhost:3000'
+    }
+});
 
+const Game = require('./Models/Game')
+const QuotableAPI = require('./QuotableAPI')
 
 mongoose.connect('mongodb://localhost:27017/typeracerTutorial',{useNewUrlParser:true,useUnifiedTopology:true},()=>{console.log('successfully connected to database')})
+io.on('connect',(socket)=>{
+    socket.emit('test', 'this is from the server');
+})
--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect} from 'react';
+import { Router, Switch, Route } from 'react-router-dom'
+import history from './history';
+import GameMenu from './components/GameMenu';
+import socket from './socketConfig'
 
 function App() {
+  useEffect(()=>{
+    socket.on('test',msg=>{
+      console.log(msg)
+    })
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router history={history}>
+        <Switch>
+          <Route path="/" exact component={GameMenu} />
+        </Switch>
+      </Router>   
     </div>
+     
   );
 }
 
