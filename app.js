d1e4b704ad0462d731a5f2d4661d821406147bc3
mnnaegel <86453692+mnnaegel@users.noreply.github.com>
2022-01-17 00:25:05 -0500

    Update app.js

--- app.js
+++ app.js
@@ -15,6 +15,27 @@ const Game = require('./Models/Game')
 const QuotableAPI = require('./QuotableAPI')
 
 mongoose.connect('mongodb://localhost:27017/typeracerTutorial',{useNewUrlParser:true,useUnifiedTopology:true},()=>{console.log('successfully connected to database')})
+
+// checks for connections to the socket from clients
 io.on('connect',(socket)=>{
-    socket.emit('test', 'this is from the server');
+    socket.on('create-game',async (nickname)=>{
+        try{
+            const quotableData = await QuotableAPI()
+            let game = new Game();
+            game.words = quotableData;
+            let player = {
+                socketID:socket.id,
+                isPartyLeader: true,
+                nickName: nickname
+            }
+            game.players.push(player)
+            game = await game.save()
+            
+            const gameID = game._id.toString();
+            socket.join(gameID)
+            io.to(gameID).emit('updateGame',game)
+        }catch(err){
+            console.log(err)
+        }
+    })
 })
